7a2360b26db0627aa60154cce4a2eab90ee568bf
Mohsin Hijazee <mh@dedomenon.org>
2009-01-13 20:03:54 +0500

    Will be used to report 404

---
+++ src/it/zeropoint/jedomenon/rest/exceptions/ResourceNotFound.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.zeropoint.jedomenon.rest.exceptions;
+
+/**
+ *
+ * @author Mohsin Hijazee
+ */
+public class ResourceNotFound extends RestException{
+
+}
