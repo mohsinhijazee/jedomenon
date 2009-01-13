3d73d74af04c15c239ef0b0f6d7f199fb7b24424
Mohsin Hijazee <mh@dedomenon.org>
2009-01-13 20:00:44 +0500

    Now all the four HTTP methods are supported

--- src/it/zeropoint/jedomenon/rest/Database.java
+++ src/it/zeropoint/jedomenon/rest/Database.java
@@ -15,23 +15,19 @@ package it.zeropoint.jedomenon.rest;
 import java.io.IOException;
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpConnection;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpState;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.params.HttpClientParams;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 
 
@@ -55,29 +51,16 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class Database {
 
   // These must be moved to an upper level
-  public  String base_url = "http://localhost:3000";
+  public  String baseURL = "http://localhost:3000";
   public  String path = "/databases";
   public  String format = "json";
+  
+  public static HttpClient httpClient = new HttpClient();
   protected JSONObject object;
   
-  protected void initialize()
-  {
-    if(object == null)
-      object = new JSONObject();
-    try
-    {
-      object.put("url", "");
-      object.put("account_url", "");
-      object.put("entities_url", "");
-      object.put("details_url", "");
-      object.put("name", "");
-      object.put("lock_version", 0);
-    }
-    catch(JSONException e)
-    {
-      
-    }
-  }
+  
+  
+
   // <editor-fold defaultstate="collapsed" desc="Constructors">
   /**
    * Default constructor
@@ -131,6 +114,82 @@ public class Database {
   // </editor-fold>
   
   // <editor-fold defaultstate="collapsed" desc="Might be to Base">
+  
+  protected void initialize()
+  {
+    if(object == null)
+      object = new JSONObject();
+    try
+    {
+      object.put("url", "");
+      object.put("account_url", "");
+      object.put("entities_url", "");
+      object.put("details_url", "");
+      object.put("name", "");
+      object.put("lock_version", 0);
+    }
+    catch(JSONException e)
+    {
+      
+    }
+  }
+  
+  /**
+   * Executes the given HTTP method on given URL with optionally supplied data.
+   * 
+   * <p>
+   * This method would execute given HTTP method on the given URL and would return
+   * the HttpMethod object after execution.
+   * </p>
+   * @param url on which to execute method
+   * @param method HTTP method name like <code>GET</code>, <code>POST</code>,
+   *        <code>PUT</code>, <code>DELETE</code>. Not case sensitive
+   * @paatram data is an array of <code>NameValuePair</code>. Currently only sensible
+   *        in case of <code>PUT</code> and <code>POST</code> methods.
+   * @return HttpMethod object of specific type depending on method requested 
+   *         to be executed. 
+   *        <li><code>GET</code> would return <code>GetMethod</code></li>
+   *        <li><code>POST</code> would return <code>PostMethod</code></li>
+   *        <li><code>PUT</code> would return <code>PutMethod</code></li>
+   *        <li><code>DELETE</code> would return <code>DeleteMethod</code></li>
+   * @throws java.io.IOException
+   * @throws org.apache.commons.httpclient.HttpException
+   */
+  protected static synchronized HttpMethod executeMethod(String url, String method, NameValuePair[] data) throws IOException, HttpException
+  {
+    
+    HttpMethod httpMethod = null;
+    
+    if(method.equalsIgnoreCase("GET"))
+    {
+      httpMethod = new GetMethod(url);
+    }
+    
+    if(method.equalsIgnoreCase("PUT"))
+    {
+      httpMethod = new PutMethod(url);
+    }
+    
+    if(method.equalsIgnoreCase("POST"))
+    {
+      httpMethod = new PostMethod(url);
+      ((PostMethod)httpMethod).setRequestBody(data);
+    }
+    
+    if(method.equalsIgnoreCase("DELETE"))
+    {
+      httpMethod = new DeleteMethod(url);
+    }
+    
+    // Required for DELETE where lock_version needed.
+    // Also required for PUT
+    if(data != null && !method.equalsIgnoreCase("POST"))
+      httpMethod.setQueryString(data);
+    
+    httpClient.executeMethod(httpMethod);
+    return httpMethod;
+  }
+  
   /**
    * Gets the attribute of given name
    * @param field name to get
@@ -159,115 +218,91 @@ public class Database {
    * @return
    * @throws org.json.JSONException
    */
-  public String url() throws JSONException
+  public String url()
   {
-    return object.getString("url");
+    return object.optString("url");
   }
   
-  public boolean delete() throws JSONException
+  public boolean delete() throws JSONException, HttpException, IOException         
   {
-    String url = this.url();
-    HttpClient client = new HttpClient();
-    HttpMethod method = new DeleteMethod(url);
+    DeleteMethod method = null;
     
+    NameValuePair[] data =  {
+                              new NameValuePair("lock_version", 
+                                      Integer.toString(lock_version()))        
+                            };
+    method = (DeleteMethod)executeMethod(this.url(), "DELETE", data);
     
-    method.setQueryString("lock_version=" + this.getAttribute("lock_version").toString());
-    
-    try
+    if(method.getStatusCode() != HttpStatus.SC_OK)
     {
-      client.executeMethod(method);
-      if(method.getStatusCode() != HttpStatus.SC_OK)
-      {
-        System.err.println("Deletion failed: " + method.getStatusLine());
-        System.err.println("Deletion failed: " + new String(method.getResponseBody()));
-      }
-    }catch(HttpException e)
-    {
+      String msg = "DELETE failed: " + method.getStatusLine();
+      msg += "Message: " + new String(method.getResponseBody());
+      System.err.println(msg);
       return false;
-    }
-    catch(IOException e)
-    {
-      return false;
-    }
-    finally
-    {
-      method.releaseConnection();      
     }
     
     return true;
+  
   }
   
-  public boolean put() throws JSONException
-  {
-    
-
-    HttpClient client = new HttpClient();
-    PutMethod method = new PutMethod(this.url());
+  public JSONObject put() throws JSONException
+  { 
     NameValuePair[] data = {new NameValuePair("database", this.object.toString())};
-    method.setQueryString(data);
-    
-    
-    
-    
-
+    PutMethod method = null;
+    JSONObject resource = null;
     
     try
     {
-      client.executeMethod(method);
+      method = (PutMethod) executeMethod(this.url(), "PUT", data); 
       
       if(method.getStatusCode() != HttpStatus.SC_OK)
       {
         System.err.println("PUT failed!: " + method.getStatusLine());
         System.err.println("PUT failed!: " + new String(method.getResponseBody()));
+        return null;
       }
-    }
-    catch(HttpException e)
-    {
-      return false;
-    }
-    catch(IOException e)
-    {
+      
+      resource = new JSONObject(new String(method.getResponseBody()));
       
     }
-    finally
-    {
-      method.releaseConnection();
-    }
-    
-    return true;
-  }
-  
-  public boolean post()
-  {
-    HttpClient client = new HttpClient();
-    PostMethod method = new PostMethod(base_url + path + format);
-    NameValuePair[] data = {new NameValuePair("database", object.toString())};
-    method.setRequestBody(data);
-    
-    try
-    {
-      client.executeMethod(method);
-      if(method.getStatusCode() != HttpStatus.SC_CREATED)
-      {
-        String msg = "POST Failed: " + method.getStatusLine() + "\n";
-        msg += new String(method.getResponseBody());
-        System.err.println(msg);
-      }
-    }
     catch(HttpException e)
     {
-      System.err.println(e.toString());
+      return null;
     }
     catch(IOException e)
     {
-      System.err.println(e.toString());
+      return null;
     }
     finally
     {
       method.releaseConnection();
     }
-            
-    return true;
+    
+    return resource;
+  }
+  
+  public JSONObject post() throws JSONException, HttpException, IOException
+  {
+    
+    NameValuePair[] data = {new NameValuePair("database", object.toString())};
+    PostMethod method = null;
+    
+  
+    method = (PostMethod)executeMethod(baseURL + path + "." + format,
+            "POST", data);
+      
+    if(method.getStatusCode() != HttpStatus.SC_CREATED)
+    {
+      String msg = "POST Failed: " + method.getStatusLine() + "\n";
+      msg += new String(method.getResponseBody());
+      System.err.println(msg);
+      return null;
+    }
+      
+    String json = new String(method.getResponseBody());
+    JSONArray array = new JSONArray(json);
+    method.releaseConnection();
+    return fromURL(array.getString(0));      
   }
   
   public static boolean delete(String url)
@@ -277,7 +312,7 @@ public class Database {
   
   public static boolean delete(int id)
   {
-    throw  new NotImplementedException();
+    return false;
   }
 
   public String getRaw(int id)
@@ -286,7 +321,7 @@ public class Database {
     String resource = "";
     
     HttpClient client  = new HttpClient();
-    HttpMethod method = new GetMethod(base_url + path + "/" + id + "." + format);
+    HttpMethod method = new GetMethod(baseURL + path + "/" + id + "." + format);
     method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
             new DefaultHttpMethodRetryHandler(3, false));
     
@@ -334,7 +369,7 @@ public class Database {
   {
     String databases = "";
     HttpClient client  = new HttpClient();
-    HttpMethod method = new GetMethod(base_url + "/databases.json");
+    HttpMethod method = new GetMethod(baseURL + "/databases.json");
     method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
             new DefaultHttpMethodRetryHandler(3, false));
     
@@ -390,24 +425,38 @@ public class Database {
   // </editor-fold>
   
   // <editor-fold defaultstate="collapsed" desc="Factory methods">
-  public static Database fromURL(String url)
+  public static synchronized  JSONObject fromURL(String url) throws JSONException, HttpException, IOException
   {
-    throw  new NotImplementedException();
+    
+    GetMethod method = null;
+    JSONObject obj = null;
+    
+    method = (GetMethod) executeMethod(url, "GET", null);
+      
+    if(method.getStatusCode() != HttpStatus.SC_OK)
+    {
+      System.err.println(new String(method.getResponseBody()));
+      return null;
+    }
+    
+    obj = new JSONObject(new String(method.getResponseBody()));
+    
+    return obj;
   }
   
-  public static Database fromID(int id)
+  public static JSONObject fromID(int id)
   {
-    throw  new NotImplementedException();
+    return null;
   }
   
   public static Database fromJSON(String json)
   {
-    throw  new NotImplementedException();
+    return null;
   }
   
   public static Database fromJSONObject(JSONObject object)
   {
-    throw  new NotImplementedException();
+    return null;
   }
   // </editor-fold>
 
@@ -439,12 +488,17 @@ public class Database {
   
   public Entity[] getEntities()
   {
-    throw  new NotImplementedException();
+    return null;
   }
   
   public Detail[] getDetails()
   {
-    throw  new NotImplementedException();
+    return null;
+  }
+  
+  public int lock_version()
+  {
+    return this.object.optInt("lock_version");
   }
   // </editor-fold>
   
