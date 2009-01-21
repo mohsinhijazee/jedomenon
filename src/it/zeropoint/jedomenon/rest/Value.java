7ef1b68931d09b8a9202659fce63210dcdf87c6e
Mohsin Hijazee <mh@dedomenon.org>
2009-01-21 18:45:58 +0500

    Initial addition

---
+++ src/it/zeropoint/jedomenon/rest/Value.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.zeropoint.jedomenon.rest;
+
+/**
+ *
+ * @author mohsinhijazee
+ */
+public class Value {
+
+}
