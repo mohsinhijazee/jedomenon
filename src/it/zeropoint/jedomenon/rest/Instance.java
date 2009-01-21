e13f3f495725888fe6743f669f853bfcd38da281
Mohsin Hijazee <mh@dedomenon.org>
2009-01-21 18:45:12 +0500

    Initial skeleton. Yet need to decide how the values would be represented. Specific methdos are added though

---
+++ src/it/zeropoint/jedomenon/rest/Instance.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.zeropoint.jedomenon.rest;
+
+/**
+ * Represents an Instance
+ * @author Mohsin Hijazee
+ */
+public class Instance{
+
+  
+  
+  // <editor-fold defaultstate="collapsed" desc="Specific Methods"> 
+  public String getDetailsURL()
+  {
+    return "";
+  }
+  
+  public String getLinksURL()
+  {
+    return "";
+  }
+  
+  public String getEntityURL()
+  {
+   return "";  
+  }
+  
+  public void setEntityURL(String url)
+  {
+    
+  }
+  
+  public Detail[] getDetails()
+  {
+    return null;
+  }
+  
+  public Link[] getLinks()
+  {
+    return null;
+  }
+  public Entity getEntity()
+  {
+    return null;
+  }
+  
+  public void setEntity(Entity entity)
+  {
+    
+  }
+  // </editor-fold> Specific Methods
+  
+}
